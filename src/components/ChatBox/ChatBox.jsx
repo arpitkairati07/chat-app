eae42a15bbd9e14315b5f7ba486b34b305616a92
arpitkairati2002 <arpitkairati54@gmail.com>
2025-01-26 10:52:57 +0000

    chatbox

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -10,6 +10,40 @@ const ChatBox = () => {
           <p>John Doe <img className='dot' src={assets.green_dot} alt="" srcset="" /></p>
           <img src={assets.help_icon} alt="" className='help'/>
       </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error vero fugiat at.</p>
+          <div>
+            <img src={assets.profile_img} alt="" className='help'/>
+            <p>4:00</p>
+          </div>
+        </div>
+
+        <div className="s-msg">
+          <img className='msg-img' src={assets.pic1} alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" className='help'/>
+            <p>4:00</p>
+          </div>
+        </div>
+
+        <div className="r-msg">
+          <p className="msg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error vero fugiat at.</p>
+          <div>
+            <img src={assets.profile_img} alt="" className='help'/>
+            <p>4:00</p>
+          </div>
+        </div>
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder='Send a message'/>
+        <input type="file" name="" id="image" accept='image/png , image/jpeg' hidden/>
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
     </div>
   )
 }
